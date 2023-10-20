7a5500c3a5dec583b3b6e194fd311e50828992ef
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:11:15 +0200

    Add Resume data and model

---
+++ ResumeApp/Models/Resume.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+namespace ResumeApp.Models;
+
+public record ResumeModel
+{
+    public ResumeBasicsModel Basics { get; set; }
+    public List<ResumeEducationModel> Education { get; set; }
+    public List<ResumeCertificationModel> Certificates { get; set; }
+    public List<ResumeLanguageModel> Languages { get; set; }
+}
+
+public record ResumeBasicsModel
+{
+    public string Name { get; set; }
+    public string Label { get; set; }
+    public string Picture { get; set; }
+    public string Email { get; set; }
+    public DateTime? DateBirth { get; set; }
+}
+
+public record ResumeEducationModel
+{
+    public string School { get; set; }
+    public string Degree { get; set; }
+    public string Graduated { get; set; }
+    public string Description { get; set; }
+}
+
+public record ResumeCertificationModel
+{
+    public string Name { get; set; }
+    public string Date { get; set; }
+    public string Issuer { get; set; }
+}
+
+public record ResumeLanguageModel
+{
+    public string Name { get; set; }
+    public string Level { get; set; }
+}
